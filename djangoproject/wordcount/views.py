7d84870af98e289543fc8f824102fdeb112b3b66
dudskchl <dhsmfdms100@naver.com>
2019-05-27 17:15:04 +0900

    first commit of wordcount proj

--- djangoproject/wordcount/views.py
+++ djangoproject/wordcount/views.py
@@ -19,3 +19,4 @@ def result(request):
             word_dictionary[word]=1
 
     return render(request, 'result.html', { 'full' : text, 'total':len(words), 'dictionary' :word_dictionary.items() })
+
